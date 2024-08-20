1fddb5106ce42309a42ef3304c84188abedbc13b
Joshua Chua <joshuachua2000@hotmail.com>
2024-08-20 22:33:27 +1000

    Fix some formatting

--- ReLU.py
+++ ReLU.py
@@ -1,14 +1,17 @@
 from numpy import np
 class ReLU:
 
+    """Returns 0 if neuron value < 0"""
     @staticmethod
     def activate(neuron_value):
-        """Returns 0 if neuron value < 0"""
+        
         return np.maximium(0, neuron_value)
     
+
+    """Computes the gradient of the loss with respect to the input for ReLU."""
     @staticmethod
     def backward(output_gradient, neuron_value):
-        """Computes the gradient of the loss with respect to the input for ReLU."""
+
         # Use the stored linear_neuron_values from the forward pass
         return output_gradient * (neuron_value > 0)
     
