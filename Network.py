88ec91b7870d4332192639377770912a517dfeae
Joshua Chua <joshuachua2000@hotmail.com>
2024-11-07 18:27:32 +0800

    1. Fixed out of bounds error in the creation of the array\n2. Changed the logic of the instantiation of the Network class\n3. Introduced a Settings.py which serves as a file that contains static variables to decouple the logic and tuning of the neural network.\n4. Added Input and Output Layer to layer array.

--- Network.py
+++ Network.py
@@ -1,45 +1,54 @@
 import Layer
+import Settings
 class NeuralNetwork:
     """
     Neural Network class -> Serves as a wrapper for all the layers
     Create input layer -> hidden layers -> output layer
     """
 
-    def __init__(self, input_dims:int, output_dims:int, hidden_layers:int, hidden_layer_dims:int):
+    def __init__(self):
 
-        #Declare number of neurons in the input layer
-        self.input_dims = input_dims
+        # Declare number of neurons in the input layer
+        self.input_dims = Settings.IN_DIMS
 
-        #Declare number of neurons in the output layer
-        self.output_dims = output_dims
+        # Declare number of neurons in the output layer
+        self.output_dims = Settings.OUT_DIM
 
-        #Declare number of hidden layers
-        self.hidden_layers = hidden_layers
+        # Declare number of hidden layers
+        self.hidden_layers = Settings.HIDDEN_LAYERS
 
-        #Declare number of neurons in hidden layers
-        self.hidden_layers_dim = hidden_layer_dims 
+        # Declare number of neurons in hidden layers
+        self.hidden_layers_dim = Settings.HIDDEN_LAYER_DIM
 
-        #To Store all the layers within a single array
+        # To Store all the layers within a single array
         self.layer_array = []
 
-        #Create input layer
+        # Create input and add input layer to array
         self.input_layer: Layer = Layer(None, self.input_dims, is_input=True, is_output=False)
+        self.layer_array.append(self.input_layer)
 
-        #Check if any hidden layers are requested
-        if hidden_layers != 0:
+        # Create Hidden Layers
+        if self.hidden_layers > 0:
 
             #Create and connect hidden layers
-            for idx in range(1,hidden_layers):
-                
+            for idx in range(1,self.hidden_layers):
+
+                # Gain reference to layer in array
                 previous_layer = self.layer_array[idx - 1]
+
+                # Create the new hidden array ( hooking up the previous array )
                 new_hidden_layer = Layer(previous_layer, self.hidden_layers_dim, is_input=False, is_output=False)
-                self.layer_array[idx] = new_hidden_layer
+                
+                # Add this Layer to the array
+                self.layer_array.append(new_hidden_layer)
             
-        #Create output layer
+        #Create and add output layer
         self.output_layer : Layer = Layer(self.layer_array[-1], self.output_dims, is_input=False, is_output=True)
+        self.layer_array.append(self.output_layer)
 
     def forward(self):
         """To feed data forward and get the predicted result"""
+
         pass
 
     def back_propagate(self):
---
+++ Settings.py
@@ -0,0 +1,11 @@
+
+# Number of Neurons in the input layer
+IN_DIMS = 100
+
+# Number of Neurons in the output layer
+OUT_DIM = 1
+
+# Number of Hidden Layers
+LAYERS_DIMS = 5
+
+
