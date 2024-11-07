Fixed Parameter passed in softmax function: output_layer

Softmax.py
@@ -9,9 +9,12 @@ class Softmax:
     """
     @staticmethod
     def activate(output_layer : Layer):
+
+        #Neuron Array
+        neuron_array = output_layer.neuron_value
  
         #This is to make sure that the values aren't too big when doing the exponentitaion
-        adjusted_neuron_values = np.exp(output_layer - np.max(output_layer, axis=1, keepdims=True))  # Stability improvement
+        adjusted_neuron_values = np.exp(neuron_array - np.max(neuron_array, axis=1, keepdims=True))  # Stability improvement
         
         #Turning the array of neuron values into probabilities
         probabilities = adjusted_neuron_values / np.sum(adjusted_neuron_values, axis=1, keepdims=True)